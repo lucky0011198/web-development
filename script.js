557ed5546146888f6cada920a9e6f1c3a68752bd
lucky0011198 <68720142+lucky0011198@users.noreply.github.com>
2021-05-24 12:15:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $(".toggle").click(function () {
+    $(".text").toggle("slow");
+  });
+
+  $("#popoverData").popover();
+  $("#popoverOption").popover({ trigger: "hover" });
+  $("#popoverData1").popover();
+  $("#popoverData2").popover();
+  $("#popoverOption").popover({ trigger: "hover" });
+});
+function on() {
+  document.getElementById("on").style.fill = "url(#linear-gradient)";
+}
+function off() {
+  document.getElementById("on").style.fill = "#909090";
+}
+var i = 0;
+function myFunction() {
+  i++;
+  document.getElementById("n").innerHTML += i + "<br>";
+  var k = document.getElementById("myText").value;
+  document.getElementById("print").innerHTML += k + "<br>";
+
+  var k1 = document.getElementById("myText1").value;
+  document.getElementById("print1").innerHTML += k1 + "<br>";
+  var k2 = document.getElementById("myText2").value;
+  document.getElementById("print2").innerHTML += k2 + "<br>";
+}
